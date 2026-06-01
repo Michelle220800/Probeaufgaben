d02b4f6d3ec64f8a5cfbdb8902f3933bf093dddc
Michelle-Jane Kruse <142615498+Midolix@users.noreply.github.com>
2026-06-01 20:28:42 +0200

    Updated

--- Aufgabe1.php
+++ Aufgabe1.php
@@ -1,1 +1,15 @@
 <?php
+
+//Gesucht: Welche 10 Lizenzen versuchen am häufigsten, auf den Server zuzugreifen. Wie oft versuchen sie auf den Server zuzugreifen
+/*Benötigt: - Logfile das eingelesen wird
+            - Seriennummer aus jeder Zeile extrahieren (foreach?)
+            - Array um die Zugriffe zu zählen
+            - Ergebnisse sortieren (Schauen wie man sie absteigend sortiert)
+            - Die ersten 10 Einträge mit den höchsten Zugriffen ausgeben */
+
+//Leeres Array erstellen. Für die Speicherung von Seriennummern und Anzahl der Zugriffe
+$counter = [];
+
+$lines = file("access.log");
+
+//Schleife um jede Zeile in der Datei auszulesen
